bfea44334fe8eb847e03adbcaf5b70b99deaba73
Oscar Romero <oscalito992@gmail.com>
2024-06-02 20:52:39 -0400

    Initial commit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.diceroller.ui.theme.DiceRollerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DiceRollerTheme {
+                DiceRollerApp()
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun DiceRollerApp() {
+    DiceWithButtonAndImage(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    )
+}
+
+private fun rollDice() : Int {
+    return (1..6).random()
+}
+
+private fun getDiceImage(result: Int) : Int {
+    return when(result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+}
+
+@Composable
+private fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+
+    var result by remember { mutableIntStateOf(1) }
+    val imageResource = getDiceImage(result)
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(imageResource),
+            contentDescription = result.toString()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = { result = rollDice() } ) {
+            Text(text = stringResource(id = R.string.roll))
+        }
+    }
+}
